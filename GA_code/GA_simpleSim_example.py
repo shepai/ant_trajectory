cd45e03074c3e77a9597d39a68f3c527c93aeb08
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-04-26 19:46:05 +0200

    Update GA_simpleSim_example.py

--- GA_code/GA_simpleSim_example.py
+++ GA_code/GA_simpleSim_example.py
@@ -10,8 +10,8 @@ env=environment() #call in demo environment
 
 #setup the network parameters so that the input is all the pixels values and output 2 velocities for motors
 _INPUT_SIZE_=env.getimage().flatten().shape[0]+2 #the 2 is for the target location, but realistically it will not know where the target is
-_H1_=10
-_H2_=5
+_H1_=36
+_H2_=6
 _OUTPUTSIZE_=2
 
 def fitness(trajectory,targets):
@@ -19,13 +19,14 @@ def fitness(trajectory,targets):
     #so we can use the fitness as a sum of all values as it increases
     correct=np.sum(np.diff(targets)*-1)
     fitness=correct/len(np.arange(0,1,0.1)) #total moves getting closer / total time
+    if fitness<0: fitness=0
     return fitness
 
 #microbial GA
-ga=Microbial_GA(10000,10,0.2,sex=1) #
+ga=Microbial_GA(1,10,0.2,sex=1) #
 ga.initialize_population(controller,[_INPUT_SIZE_,[_H1_,_H2_],_OUTPUTSIZE_])
 history,fitness=ga.evolve(env,fitness,outputs=True) #run the GA
-
+print(fitness.shape)
 #from here you will want to find the best one from ga population from our fitness matrix
 best_genotype=ga.pop[np.argmax(fitness)]
 env.runTrial(best_genotype)
