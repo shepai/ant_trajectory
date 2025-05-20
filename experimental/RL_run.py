01ccbb0638d9f78c60d54baaf014327306bfaa89
shepai <dextershepherd@hotmail.co.uk>
2025-05-20 14:46:15 +0000

    better for saving

--- experimental/RL_run.py
+++ experimental/RL_run.py
@@ -5,39 +5,61 @@ from grid_environment import environment
 from RL_code.DQNAgent import DQNAgent
 import numpy as np
 import datetime 
-env=environment() #call in demo environment
-image=env.getAntVision()
-agent=DQNAgent(image.shape,3,"cuda")
+import os
+def save_array_to_folder(base_dir, folder_name, fitness, pathways):
+    folder_path = os.path.join(base_dir, folder_name)
+    # Create folder if it doesn't exist
+    os.makedirs(folder_path, exist_ok=True)
+    # Save the array
+    np.save(folder_path+"/fitnesses", fitness)
+    
+    transform_model_trajects(pathways, 
+        image_path="/its/home/drs25/ant_trajectory/trajectory_code/testA_ant1_image.jpg", savefig=folder_path+"pathsTaken.pdf", x_scale=1)
+    max_v = max(arr.shape[0] for arr in pathways)
+    padded_pathways = []
+    for arr in pathways:
+        pad_length = max_v - arr.shape[0]
+        if pad_length > 0:
+            padded = np.vstack([arr, np.zeros((pad_length, 2))])
+        else:
+            padded = arr
+        padded_pathways.append(padded)
+    routes = np.stack(padded_pathways)
+    np.save(folder_path+"/routes", routes)
+    
+    print(f"Array saved to: {folder_path}")
 
-T=1
-dt=0.01
-env.dt=dt
-path_hist=[]
-for j in range(3000):
-    env.reset()
-    state= env.getAntVision()
-    total_reward=0
-    for t in np.arange(0,T,dt):
-        action = agent.step(state)  # pick action using policy
-        next_state, reward, done, _ = env.step(action)  # env reacts
-        agent.store_transition(state, action, reward, next_state, done)  # remember
-        agent.train_step()  # learn from experience
-        state = next_state
-        total_reward+=reward
-        if done: break
-    path_hist.append(np.array(env.trajectory).copy())
-    print("Trial",j,"Reward:",total_reward)
+def run(experiment_name,epochs):
+    env=environment() #call in demo environment
+    image=env.getAntVision()
+    agent=DQNAgent(image.shape,3,"cuda")
 
-date=str(datetime.datetime.now()).replace(":","_")
-##########
-transform_model_trajects(path_hist, 
-    image_path="/its/home/drs25/ant_trajectory/trajectory_code/testA_ant1_image.jpg", savefig="/its/home/drs25/ant_trajectory/data/RL_TRIAL/show"+date+".pdf", x_scale=1)
-p=[]
-for path in path_hist:
-    if (len(np.arange(0,T,dt))-len(path)) > 0 :
-        p.append(path+[[0,0]]*(len(np.arange(0,T,dt))-len(path)))
-    else: p.append(path)
-np.save("/its/home/drs25/ant_trajectory/data/RL_TRIAL/data"+str(date),np.array(p))
+    T=1
+    dt=0.01
+    env.dt=dt
+    path_hist=[]
+    reward_hist=[]
+    for j in range(epochs):
+        env.reset()
+        state= env.getAntVision()
+        total_reward=0
+        for t in np.arange(0,T,dt):
+            action = agent.step(state)  # pick action using policy
+            next_state, reward, done, _ = env.step(action)  # env reacts
+            agent.store_transition(state, action, reward, next_state, done)  # remember
+            agent.train_step()  # learn from experience
+            state = next_state
+            total_reward+=reward
+            if done: break
+        reward_hist.append(total_reward)
+        path_hist.append(np.array(env.trajectory).copy())
+        print("Trial",j,"Reward:",total_reward)
+
+    save_array_to_folder("/its/home/drs25/ant_trajectory/data/RL_TRIAL/", experiment_name, np.array(reward_hist), path_hist)
+
+if __name__=="__main__":
+    run("test",20)
+
 
 """
 
